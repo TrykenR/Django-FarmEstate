e6cd7a0075c3fe106089a650bf43b3c1cb900ccb
TrykenR <andrescgd17@gmail.com>
2025-10-21 16:17:05 -0500

    Resubiendo modelos

--- farmEstate/farmApp/models.py
+++ farmEstate/farmApp/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-from django.db import models
-
-
 class TablaDatos(models.Model):
     nombre = models.CharField(max_length=20)
     jerarquia = models.IntegerField()
