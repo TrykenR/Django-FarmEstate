05fbb9454a8696c2abf84cb500eb43e0535e03d8
TrykenR <andrescgd17@gmail.com>
2025-11-18 21:26:11 -0500

    cambio de estilos

--- farmEstate/farmApp/views.py
+++ farmEstate/farmApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Trabajadores, Animales, Huertos, Producciones
 from .forms import TrabajadoresForm, AnimalesForm, HuertosForm, ProduccionesForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'farmApp/home.html')
@@ -34,3 +36,29 @@ def crear_animal(request):
     else:
         form = AnimalesForm()
     return render(request, 'farmApp/animal_form.html', {'form': form})
+def login_trabajador(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect("dashboard")
+        else:
+            return render(request, "farmApp/login.html", {
+                "error": "Credenciales incorrectas"
+            })
+
+    return render(request, "farmApp/login.html")
+
+
+@login_required
+def dashboard(request):
+    return render(request, "farmApp/dashboard.html")
+
+
+def salir(request):
+    logout(request)
+    return redirect("login")
